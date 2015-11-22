0dc3c9da9fe7c67a2a145bb2f3bc4161d7eda7b0
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2015-11-22 12:06:52 +0900

    Sign regardless of whether SNAPSHOT or not

--- publish.sh
+++ publish.sh
@@ -2,8 +2,9 @@
 
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     echo $PASSPHRASE | gpg --output $GPG_SECRETKEYRING --batch --passphrase-fd 0 --decrypt encrypted-maven.gpg
-    ./mvnw --settings settings.xml clean deploy \
-        -DperformRelease=true \
+    ./mvnw --settings settings.xml deploy \
+        -DskipTests \
+        -DperformSign=true \
         -Dgpg.keyname=$GPG_KEYNAME \
         -Dgpg.passphrase=$GPG_PASSPHRASE \
         -Dgpg.secretKeyring=$GPG_SECRETKEYRING
