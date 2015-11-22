58d8b50c1e87b67d45877a46849b08a0a0cd6dad
Toshihiro Nakamura <toshihiro.nakamura@gmail.com>
2015-11-22 11:35:59 +0900

    Suppressing GPG signing as default

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@
 if [[ $TRAVIS_PULL_REQUEST == "false" ]] && [[ $TRAVIS_BRANCH == "master" ]]; then
     echo $PASSPHRASE | gpg --output $GPG_SECRETKEYRING --batch --passphrase-fd 0 --decrypt encrypted-maven.gpg
     ./mvnw --settings settings.xml clean deploy \
+        -DperformRelease=true \
         -Dgpg.keyname=$GPG_KEYNAME \
         -Dgpg.passphrase=$GPG_PASSPHRASE \
         -Dgpg.secretKeyring=$GPG_SECRETKEYRING
