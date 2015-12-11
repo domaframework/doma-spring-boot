f2c5f514498ecd1aebb39ae3358ae634ba83745d
Zenichi Amano <crow.misia@gmail.com>
2015-12-12 00:16:17 +0900

    naming / max-rows / fetch-size / query-timeout / batch-size / entity-listener-provider のパラメータを設定ファイルから取得する処理を追加

--- doma-spring-boot-autoconfigure/src/main/java/org/seasar/doma/boot/autoconfigure/DomaAutoConfiguration.java
+++ doma-spring-boot-autoconfigure/src/main/java/org/seasar/doma/boot/autoconfigure/DomaAutoConfiguration.java
@@ -1,10 +1,18 @@
 package org.seasar.doma.boot.autoconfigure;
 
+import java.util.function.Supplier;
+
 import javax.sql.DataSource;
 
 import org.seasar.doma.jdbc.Config;
+import org.seasar.doma.jdbc.ConfigSupport;
+import org.seasar.doma.jdbc.EntityListenerProvider;
+import org.seasar.doma.jdbc.JdbcLogger;
+import org.seasar.doma.jdbc.MapKeyNaming;
+import org.seasar.doma.jdbc.Naming;
 import org.seasar.doma.jdbc.SqlFileRepository;
 import org.seasar.doma.jdbc.dialect.Dialect;
+import org.seasar.doma.jdbc.entity.EntityListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.AutoConfigureAfter;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
@@ -12,6 +20,7 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.dao.support.PersistenceExceptionTranslator;
@@ -49,8 +58,47 @@ public class DomaAutoConfiguration {
 
     @Bean
     @ConditionalOnMissingBean
+    public JdbcLogger jdbcLogger() {
+        return ConfigSupport.defaultJdbcLogger;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public Naming naming() {
+        return domaProperties.getNaming().get();
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MapKeyNaming mapKeyNaming() {
+        return ConfigSupport.defaultMapKeyNaming;
+    }
+
+    @Bean
+    @ConditionalOnProperty(prefix = DomaProperties.DOMA_PREFIX, name = "entity-listener-provider-bean-factory", havingValue="true", matchIfMissing = false)
+    public EntityListenerProvider entityListenerProviderBeanFactory(final ApplicationContext context) {
+        return new EntityListenerProvider() {
+            @Override
+            public <ENTITY, LISTENER extends EntityListener<ENTITY>> LISTENER get(
+                    final Class<LISTENER> listenerClass,
+                    final Supplier<LISTENER> listenerSupplier) {
+                return context.getBean(listenerClass);
+            }
+        };
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public EntityListenerProvider entityListenerProvider() {
+        return ConfigSupport.defaultEntityListenerProvider;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
     public Config config(DataSource dataSource, Dialect dialect,
-            SqlFileRepository sqlFileRepository) {
+            SqlFileRepository sqlFileRepository, JdbcLogger jdbcLogger,
+            EntityListenerProvider entityListenerProvider, Naming naming,
+            MapKeyNaming mapKeyNaming) {
         return new Config() {
             @Override
             public DataSource getDataSource() {
@@ -67,6 +115,45 @@ public class DomaAutoConfiguration {
                 return sqlFileRepository;
             }
 
+            @Override
+            public JdbcLogger getJdbcLogger() {
+                return jdbcLogger;
+            }
+
+            @Override
+            public Naming getNaming() {
+                return naming;
+            }
+
+            @Override
+            public MapKeyNaming getMapKeyNaming() {
+                return mapKeyNaming;
+            }
+
+            @Override
+            public int getMaxRows() {
+                return domaProperties.getMaxRows();
+            }
+
+            @Override
+            public int getFetchSize() {
+                return domaProperties.getFetchSize();
+            }
+
+            @Override
+            public int getQueryTimeout() {
+                return domaProperties.getQueryTimeout();
+            }
+
+            @Override
+            public int getBatchSize() {
+                return domaProperties.getBatchSize();
+            }
+
+            @Override
+            public EntityListenerProvider getEntityListenerProvider() {
+                return entityListenerProvider;
+            }
         };
     }
 
--- doma-spring-boot-autoconfigure/src/main/java/org/seasar/doma/boot/autoconfigure/DomaProperties.java
+++ doma-spring-boot-autoconfigure/src/main/java/org/seasar/doma/boot/autoconfigure/DomaProperties.java
@@ -5,6 +5,7 @@ import static org.seasar.doma.boot.autoconfigure.DomaProperties.DOMA_PREFIX;
 import java.util.function.Supplier;
 
 import org.seasar.doma.jdbc.GreedyCacheSqlFileRepository;
+import org.seasar.doma.jdbc.Naming;
 import org.seasar.doma.jdbc.NoCacheSqlFileRepository;
 import org.seasar.doma.jdbc.SqlFileRepository;
 import org.seasar.doma.jdbc.dialect.*;
@@ -29,9 +30,39 @@ public class DomaProperties {
     private SqlFileRepositoryType sqlFileRepository = SqlFileRepositoryType.GREEDY_CACHE;
 
     /**
+     * Type of {@link Naming}.
+     */
+    private NamingType naming = NamingType.DEFAULT;
+
+    /**
+     * Max Rows
+     */
+    private int maxRows = 0;
+
+    /**
+     * Fetch Size
+     */
+    private int fetchSize = 0;
+
+    /**
+     * Query Timeout
+     */
+    private int queryTimeout = 0;
+
+    /**
+     * Batch Size
+     */
+    private int batchSize = 0;
+
+    /**
      * Whether convert {@link org.seasar.doma.jdbc.JdbcException} into {@link org.springframework.dao.DataAccessException}.
      */
     private boolean exceptionTranslationEnabled = true;
+
+    /**
+     * Get {@link org.seasar.doma.jdbc.entity.EntityListener} from {@link org.springframework.beans.factory.BeanFactory}
+     */
+    private boolean entityListenerProviderBeanFactory = false;
 
     public DialectType getDialect() {
         return dialect;
@@ -49,6 +80,46 @@ public class DomaProperties {
         this.sqlFileRepository = sqlFileRepository;
     }
 
+    public NamingType getNaming() {
+        return naming;
+    }
+
+    public void setNaming(NamingType naming) {
+        this.naming = naming;
+    }
+
+    public int getMaxRows() {
+        return maxRows;
+    }
+
+    public void setMaxRows(int maxRows) {
+        this.maxRows = maxRows;
+    }
+
+    public int getFetchSize() {
+        return fetchSize;
+    }
+
+    public void setFetchSize(int fetchSize) {
+        this.fetchSize = fetchSize;
+    }
+
+    public int getQueryTimeout() {
+        return queryTimeout;
+    }
+
+    public void setQueryTimeout(int queryTimeout) {
+        this.queryTimeout = queryTimeout;
+    }
+
+    public int getBatchSize() {
+        return batchSize;
+    }
+
+    public void setBatchSize(int batchSize) {
+        this.batchSize = batchSize;
+    }
+
     public boolean isExceptionTranslationEnabled() {
         return exceptionTranslationEnabled;
     }
@@ -56,6 +127,15 @@ public class DomaProperties {
     public void setExceptionTranslationEnabled(
             boolean exceptionTranslationEnabled) {
         this.exceptionTranslationEnabled = exceptionTranslationEnabled;
+    }
+
+    public boolean isEntityListenerProviderBeanFactory() {
+        return entityListenerProviderBeanFactory;
+    }
+
+    public void setEntityListenerProviderBeanFactory(
+            boolean entityListenerProviderBeanFactory) {
+        this.entityListenerProviderBeanFactory = entityListenerProviderBeanFactory;
     }
 
     public static enum DialectType {
@@ -92,10 +172,33 @@ public class DomaProperties {
         }
     }
 
+    public static enum NamingType {
+        DEFAULT(Naming.DEFAULT),
+        LENIENT_SNAKE_LOWER_CASE(Naming.LENIENT_SNAKE_LOWER_CASE),
+        LENIENT_SNAKE_UPPER_CASE(Naming.LENIENT_SNAKE_UPPER_CASE),
+        LOWER_CASE(Naming.LOWER_CASE),
+        NONE(Naming.NONE),
+        SNAKE_LOWER_CASE(Naming.SNAKE_LOWER_CASE),
+        SNAKE_UPPER_CASE(Naming.SNAKE_UPPER_CASE),
+        UPPER_CASE(Naming.UPPER_CASE);
+        private final Naming naming;
+
+        NamingType(Naming naming) {
+            this.naming = naming;
+        }
+
+        public Naming get() {
+            return this.naming;
+        }
+    }
+
     @Override
     public String toString() {
         return "DomaProperties{" + "dialect=" + dialect + ", sqlFileRepository="
-                + sqlFileRepository + ", exceptionTranslationEnabled="
-                + exceptionTranslationEnabled + '}';
+                + sqlFileRepository + ", naming=" + naming + ", maxRows=" + maxRows
+                + ", fetchSize=" + fetchSize + ", queryTimeout=" + queryTimeout
+                + ", batchSize=" + batchSize + ", exceptionTranslationEnabled="
+                + exceptionTranslationEnabled + ", entityListenerProviderBeanFactory="
+                + entityListenerProviderBeanFactory + '}';
     }
 }
