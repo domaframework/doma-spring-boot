13b3743da579e1cbecb053004ebafb674996bc62
Taichi Uragami <backpaper0@gmail.com>
2018-03-04 13:54:28 +0900

    Restructure project (#26)
    * Add core project
    
    * Organize dependencies
    
    * Change package

--- doma-spring-boot-core/src/main/java/org/seasar/doma/boot/DomaPersistenceExceptionTranslator.java
+++ doma-spring-boot-core/src/main/java/org/seasar/doma/boot/DomaPersistenceExceptionTranslator.java
@@ -13,7 +13,7 @@
  * either express or implied. See the License for the specific language
  * governing permissions and limitations under the License.
  */
-package org.seasar.doma.boot.autoconfigure;
+package org.seasar.doma.boot;
 
 import java.sql.SQLException;
 
